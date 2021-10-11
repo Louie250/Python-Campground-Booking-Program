587ab2a1ab862555994a11e5e41156433bb809f3
gameattack <ledoswell@gmail.com>
2021-10-11 21:43:52 +0100

    Add files via upload

---
+++ backend_functions.py
@@ -0,0 +1,53 @@
+from random import randrange
+from datetime import date
+
+pricePerNight = 7
+date = date.today().strftime("%d/%m/%Y")
+
+def addSale(date, name, nights, pricePerNight, saleValue):
+    with open('Sales.txt', 'a') as salesF:
+        salesF.write(str(date))
+        salesF.write(" - ")
+        salesF.write(str(name))
+        salesF.write(" - ")
+        salesF.write(str(nights))
+        salesF.write(" - ")
+        salesF.write(str(pricePerNight))
+        salesF.write(" - ")
+        salesF.write(str(saleValue))
+        salesF.write("\n")
+
+def addBooking(name, people, plot, nights):
+    cost = nights * pricePerNight
+    insertBooking = [name,people,"Plot "+ str(plot),nights, date, "£"+str(cost)]
+    addSale(date, name, nights, pricePerNight, cost)
+    with open('Bookings.txt', 'a') as bookings:
+        bookings.write(str(insertBooking))
+        bookings.write('\n')
+    
+    print("Details: ",insertBooking, "\n")
+
+def remBooking(plot):
+    with open("Bookings.txt","r+") as f:
+        new_f = f.readlines()
+        f.seek(0)
+        if ("Plot "+ str(plot)) in new_f:
+            if plot not in line:
+                f.write(line)
+        f.truncate()
+        print("Successfully removed listing\n")
+
+def checkPlot(plot):
+    with open("Bookings.txt","r") as f:
+        index = 0
+        for line in f:
+            index += 1
+            if ("Plot "+ str(plot)) in line:
+                print(line)
+                return True
+        if ("Plot "+ str(plot)) not in line:
+            print("Error, there is no booking at plot " + str(plot) + "\n")
+            return False
+def choosePlot():
+    plot = randrange(99) + 1 # Change the value in brackets to the amount of plots that you have
+    return plot
